69a3e428f5ddbda942d4951a15600f83941ae538
Moisés Ramírez <127533330+mergutm@users.noreply.github.com>
2025-10-14 09:58:13 -0600

    Update animacion.js

--- canvas/js/animacion.js
+++ canvas/js/animacion.js
@@ -1,3 +1,68 @@
+function rebote(){
+    // referencia al canvas
+    const canvas = document.getElementById('pelota');
+    //lapiz
+    const ctx = canvas.getContext('2d'); 
+
+    let coordX = 300;
+    let coordY = 100;
+    const radius = 25;
+    let vy = 0; // velocidad inicial en Y
+    const gravedad = 0.3;
+    const perdida = 0.94;  // perdida de energia al rebotar.
+
+    //posicion del piso - radio de la pelota. 
+    const posPiso = canvas.height - radius;  
+
+    //dibujaPelota(ctx, 300, coordX, radius);
+
+
+    function animar() {
+        //se borra todo lo anterior.
+        ctx.clearRect(0,0, canvas.width, canvas.height);
+
+        vy += gravedad;
+        coordY += vy;
+        coordX +=1;
+
+        if (coordY > posPiso ){
+            coordY = posPiso;
+            vy  *= -perdida; //rebote hacia arriba
+        }
+
+        dibujaPelota(ctx, coordY, coordX, radius);
+
+        requestAnimationFrame(animar);
+    }
+
+    animar();
+}
+
+
+function dibujaPelota(ctx,  coordY, coordX = 300, radius = 25){
+
+    ctx.beginPath();
+
+    const gradiente = ctx.createRadialGradient(
+        300 - radius/3,
+        coordY - radius/3,
+        10, 
+        300, 
+        coordY,
+        radius      
+    );
+    gradiente.addColorStop(0, '#9fcd');     // 9fcd = 99ffccdd
+    gradiente.addColorStop(1, '#43fd');
+
+    ctx.fillStyle = gradiente;
+
+    //       posX ,  posY,   radio, Angulo inicial , ang final  
+    ctx.arc(coordX, coordY, radius,  0 ,            2*Math.PI);
+    ctx.fill();
+
+}
+
+
 
 
 
@@ -46,3 +111,5 @@ function dibujarLineas() {
     }
 }
 //dibujarLineas();
+
+rebote();
